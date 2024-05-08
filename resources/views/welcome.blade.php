0e08abf6a99ead9e4bd553e6db4c30375740aac7
RafaelSvgh <salvatierravallerafael@gmail.com>
2024-05-08 14:46:37 -0400

    cambio de producto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 @foreach ($productos as $producto)
                     <article class="bg-white shadow rounded overflow-hidden">
-                         <img src="{{ $producto->imagenes[2]->ruta }}" class="aspect-[1/1] w-full object-cover object-center"> 
+                         <img src="{{ $producto->imagenes[0]->ruta }}" class="aspect-[1/1] w-full object-cover object-center"> 
                          <div class="p-4">
                             <h1 class="text-lg font-bold text-gray-700 line-clamp-2 min-h-[56px] ">
                                 {{$producto->nombre}}
