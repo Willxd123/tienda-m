3ee345a3d7556b060681833f596846089fb56cc8
RafaelSvgh <salvatierravallerafael@gmail.com>
2024-05-14 03:01:07 -0400

    reportes

--- app/Exports/NotaComprasExport.php
+++ app/Exports/NotaComprasExport.php
@@ -18,9 +18,8 @@ class NotaComprasExport implements FromCollection
     protected $fecha_ini;
     protected $fecha_fin;
 
-    public function __construct($id, $proveedor, $fecha, $monto_total, $fecha_ini, $fecha_fin)
+    public function __construct($proveedor, $fecha, $monto_total, $fecha_ini, $fecha_fin)
     {
-        $this->id = $id;
         $this->proveedor = $proveedor;
         $this->fecha = $fecha;
         $this->monto_total = $monto_total;
@@ -31,53 +30,38 @@ class NotaComprasExport implements FromCollection
 
     public function collection()
     {
-        
-        if($this->proveedor != ""){
-            $nota_compras = NotaCompra::whereBetween('fecha', [$this->fecha_ini, $this->fecha_fin])
-                          ->select('id',$this->proveedor)
-                          ->get();
+        if($this->proveedor != "" && $this->fecha == "" && $this->monto_total == "" ){
+            $nota_compras = NotaCompra::select('id',$this->proveedor)->get();
             return $nota_compras;
         }
 
-        if($this->fecha != ""){
-            $nota_compras = NotaCompra::whereBetween('fecha', [$this->fecha_ini, $this->fecha_fin])
-                          ->select('id',$this->fecha)
-                          ->get();
+        if($this->fecha != "" && $this->proveedor == "" && $this->monto_total == ""){
+            $nota_compras = NotaCompra::select('id',$this->fecha)->get();
             return $nota_compras;
         }
 
-        if($this->monto_total != ""){
-            $nota_compras = NotaCompra::whereBetween('fecha', [$this->fecha_ini, $this->fecha_fin])
-                          ->select('id',$this->monto_total)
-                          ->get();
+        if($this->monto_total != "" && $this->proveedor == "" && $this->fecha == ""){
+            $nota_compras = NotaCompra::select('id',$this->monto_total)->get();
             return $nota_compras;
         }
 
-        if($this->monto_total != "" && $this->proveedor != ""){
-            $nota_compras = NotaCompra::whereBetween('fecha', [$this->fecha_ini, $this->fecha_fin])
-                          ->select('id',$this->proveedor, $this->monto_total)
-                          ->get();
+        if($this->monto_total != "" && $this->proveedor != "" & $this->fecha == ""){
+            $nota_compras = NotaCompra::select('id',$this->proveedor, $this->monto_total)->get();
             return $nota_compras;
         }
 
-        if($this->monto_total != "" && $this->fecha != ""){
-            $nota_compras = NotaCompra::whereBetween('fecha', [$this->fecha_ini, $this->fecha_fin])
-                          ->select('id',$this->fecha, $this->monto_total)
-                          ->get();
+        if($this->monto_total != "" && $this->fecha != "" && $this->proveedor == ""){
+            $nota_compras = NotaCompra::select('id',$this->fecha, $this->monto_total)->get();
             return $nota_compras;
         }
 
-        if($this->proveedor != "" && $this->fecha != ""){
-            $nota_compras = NotaCompra::whereBetween('fecha', [$this->fecha_ini, $this->fecha_fin])
-                          ->select('id',$this->fecha, $this->proveedor)
-                          ->get();
+        if($this->proveedor != "" && $this->fecha != "" && $this->monto_total == ""){
+            $nota_compras = NotaCompra::select('id',$this->fecha, $this->proveedor)->get();
             return $nota_compras;
         }
 
-        if($this->proveedor != "" && $this->fecha != "" && $this->monto_total){
-            $nota_compras = NotaCompra::whereBetween('fecha', [$this->fecha_ini, $this->fecha_fin])
-                          ->select('id',$this->fecha, $this->proveedor, $this->monto_total)
-                          ->get();
+        if($this->proveedor != "" && $this->fecha != "" && $this->monto_total !=""){
+            $nota_compras = NotaCompra::select('id',$this->fecha, $this->proveedor, $this->monto_total)->get();
             return $nota_compras;
         }
 
--- app/Http/Controllers/Admin/ExportController.php
+++ app/Http/Controllers/Admin/ExportController.php
@@ -9,18 +9,32 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ExportController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('admin.reporte.create');
     }
 
-    // public function store(Request $request){
-    //     $proveedor = "";
-    //     $monto_total = "";
-    //     $ = "";
-    //     $proveedor = "";
-    //     $proveedor = "";
-    //     $proveedor = "";
-    //     $proveedor = "";
-    //     return Excel::download(new NotaComprasExport(), 'notaCompra.xlsx');
-    // }
+    public function store(Request $request)
+    {
+        $proveedor = "";
+        $monto_total = "";
+        $fecha = "";
+        $fecha_ini = $request->fecha_ini;
+        $fecha_fin = $request->fecha_fin;
+        if($request->has('proveedor')){
+            $proveedor = 'proveedor_id';
+        }
+
+        if($request->has('monto_total')){
+            $monto_total = 'monto_total';
+        }
+
+        if($request->has('fecha')){
+            $fecha = 'fecha';
+        }
+
+
+        return Excel::download(new NotaComprasExport($proveedor, $fecha,$monto_total, $fecha_ini, $fecha_fin), 'notaCompra.xlsx');
+    
+    }
 }
