fb793ee4eca5af0dec6c2b1ba8cf30332db70176
wilder choque quispe <didier.c.q.17@gmail.com>
2024-06-08 21:24:35 -0400

    listo pal main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,6 @@
             <!-- If we need pagination -->
             <div class="swiper-pagination"></div>
         </div>
-
     </figure>
 
     <div class="flex items-center justify-center h-screenobject-cover object-center ">
@@ -23,44 +22,37 @@
     <div class="px-4 py-3">
         <x-container>
             @auth
-                <div class="w- mx-auto">
-                    <!-- Swiper -->
-
-                    <div class="swiper-container">
-                        <div class="swiper-wrapper">
-                            @foreach ($premios as $premio)
-                                <div class="swiper-slide flex justify-center items-center">
-                                    <article class="bg-white shadow rounded overflow-hidden mx-auto" style="width: 50%;">
-                                        <div class="relative">
-                                            <img src="{{ $premio->producto->imagen }}" style="width: 80%;"
-                                                class="h-auto mx-auto rounded-t">
+            <div class="w-full mx-auto">
+                <!-- Swiper -->
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        @foreach ($premios as $premio)
+                            <div class="swiper-slide flex w-full h-full">
+                                <article class="bg-white shadow rounded overflow-hidden mx-auto flex flex-col justify-between h-72">
+                                    <div class="py-3 flex-shrink-0">
+                                        <img src="{{ $premio->producto->imagen }}" class="w-4/5 mx-auto rounded-t max-h-36 object-contain">
+                                    </div>
+                                    <div class="p-2 flex flex-col justify-between flex-grow">
+                                        <div>
+                                            <h1 class="text-lg font-bold text-gray-700 line-clamp-2 min-h-[20px]">
+                                                {{ $premio->producto->nombre }}
+                                            </h1>
+                                            <p class="text-base sm:text-lg text-gray-600 mb-2">
+                                                Puntos: {{ $premio->precio_puntos }}
+                                            </p>
                                         </div>
-                                        <div class="p-2 relative">
-                                            <div>
-                                                <h1 class="text-lg font-bold text-gray-700 line-clamp-2 min-h-[20px]">
-                                                    {{ $premio->producto->nombre }}
-                                                </h1>
-                                                <p class="text-base sm:text-lg text-gray-600 mb-2">
-                                                    Puntos: {{ $premio->precio_puntos }}
-                                                </p>
-                                            </div>
-                                            <a href="{{ route('cliente.premios.show', $premio) }}" class="btn btn-gray block w-full text-center sm:w-auto">
-                                                Ver más
-                                            </a>
-                                        </div>
-                                    </article>
-                                </div>
-                            @endforeach
-                        </div>
+                                        <a href="{{ route('cliente.premios.show', $premio) }}" class="btn btn-gray block w-full text-center sm:w-auto">
+                                            Ver más
+                                        </a>
+                                    </div>
+                                </article>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
+            </div>
+
             @endauth
-
-
-            <!-- Slider main container -->
-
-
-
             <!-- Productos -->
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
                 @foreach ($productos as $producto)
@@ -92,10 +84,54 @@
             </div>
         </x-container>
     </div>
-
-
+    <style>
+        .swiper-slide h1 {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2; /* Número de líneas que queremos mostrar */
+            -webkit-box-orient: vertical;
+        }
+    </style>
+    <!-- Swiper JS -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var swiper = new Swiper('.swiper-container', {
+                slidesPerView: 2, // Mostrará 5 premios a la vez
+                spaceBetween: 20,
+                loop: true,
+                autoplay: {
+                    delay: 3000,
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                breakpoints: {
+                    // cuando la pantalla sea <= 640px
+                    640: {
+                        slidesPerView: 2, // Mostrará 2 premios en dispositivos móviles
+                        spaceBetween: 10
+                    },
+                    // cuando la pantalla sea <= 768px
+                    768: {
+                        slidesPerView: 3, // Mostrará 3 premios en pantallas medianas
+                        spaceBetween: 15
+                    },
+                    // cuando la pantalla sea >= 1024px
+                    1024: {
+                        slidesPerView: 5, // Mostrará 5 premios en pantallas grandes
+                        spaceBetween: 20
+                    }
+                }
+            });
+        });
+    </script>
     <!-- Swiper CSS -->
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    {{-- ---------------------------------------------------------------------------------------------------------- --}}
+
     @push('css')
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     @endpush
