ac7a795b0b2a6268b207705c3a43f90ed20d4b06
RafaelSvgh <salvatierravallerafael@gmail.com>
2024-06-12 14:05:44 -0400

    arreglos

--- database/migrations/2024_06_03_121335_create_nota_ventas_table.php
+++ database/migrations/2024_06_03_121335_create_nota_ventas_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->float('monto_total');
             $table->date('fecha');
+            $table->string('factura')->nullable();
             $table->unsignedBigInteger('promotor_id');
             $table->foreign('promotor_id')->references('id')->on('promotors');
             $table->timestamps();
