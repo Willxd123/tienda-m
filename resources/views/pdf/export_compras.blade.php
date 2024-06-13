5c0c5b81fd8843b8659f06d5a34eef2179c2f4af
RafaelSvgh <salvatierravallerafael@gmail.com>
2024-06-13 04:53:19 -0400

    export

--- resources/views/pdf/export_compras.blade.php
+++ resources/views/pdf/export_compras.blade.php
@@ -1,40 +1,61 @@
-<table>
-    <thead>
-        @if ($proveedores->isEmpty())
-            <tr>
-                @foreach ($campos as $campo)
-                    <th>{{ $campo }}</th>
-                @endforeach
-            </tr>
-        @else
-            <tr>
-                @foreach ($campos as $campo)
-                    <th>{{ $campo }}</th>
-                @endforeach
-                <th>proveedor</th>
-            </tr>
-        @endif
+<!DOCTYPE html>
+<html lang="en">
 
-    </thead>
-    <tbody>
-        @if ($proveedores->isEmpty())
-            @foreach ($nota_compras as $nota_compra)
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        th, tr{
+            padding-left: 4px; 
+            padding-right: 4px; 
+        }
+
+    </style>
+</head>
+
+<body>
+    <table>
+        <thead>
+            @if ($proveedores->isEmpty())
                 <tr>
                     @foreach ($campos as $campo)
-                        <td>{{ $nota_compra->$campo }}</td>
+                        <th>{{ $campo }}</th>
                     @endforeach
                 </tr>
-            @endforeach
-        @else
-            @foreach ($nota_compras as $key => $nota_compra)
+            @else
                 <tr>
                     @foreach ($campos as $campo)
-                        <td>{{ $nota_compra->$campo }}</td>
+                        <th>{{ $campo }}</th>
                     @endforeach
-                    <td>{{ $proveedores[$key]->nombre }}</td>
+                    <th>proveedor</th>
                 </tr>
-            @endforeach
-        @endif
+            @endif
 
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @if ($proveedores->isEmpty())
+                @foreach ($nota_compras as $nota_compra)
+                    <tr>
+                        @foreach ($campos as $campo)
+                            <td>{{ $nota_compra->$campo }}</td>
+                        @endforeach
+                    </tr>
+                @endforeach
+            @else
+                @foreach ($nota_compras as $key => $nota_compra)
+                    <tr>
+                        @foreach ($campos as $campo)
+                            <td>{{ $nota_compra->$campo }}</td>
+                        @endforeach
+                        <td>{{ $proveedores[$key]->nombre }}</td>
+                    </tr>
+                @endforeach
+            @endif
+
+        </tbody>
+    </table>
+</body>
+
+</html>
